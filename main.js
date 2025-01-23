195861fa39e02336ff934edfb205a4caec97d3b8
Debi Lattanzi <deboralattanzi@hotmail.com>
2025-01-23 15:54:37 -0300

    agregado de funciones y dealle del simulador en HTML

--- main.js
+++ main.js
@@ -1,28 +1,55 @@
+let turnoIngresado;
+const turno = [1, 2];
+
 function saludar() {
-    let usuario = prompt("Ingrese su Nombre y apellido");
+    let usuario = prompt("Ingrese su Nombre y Apellido");
     alert("Bienvenido/a a Farmacia Social " + usuario);
 
-    const turno = [1, 2];
-    let turnoIngresado;
-
-    if (turnoIngresado = parseInt(prompt("Ingrese 1 para atención por OBRA SOCIAL o ingrese 2 para atención PARTICULAR"))) {
+    if (turnoIngresado = parseInt(prompt("Ingrese 1 para atención por OBRA SOCIAL o ingrese 2 para atención PARTICULAR."))) {
 
         while (turnoIngresado != turno[0] && turnoIngresado != turno[1]) {
             alert("Por favor ingrese 1 o 2");
-            turnoIngresado = parseInt(prompt("Por favor, ingrese 1 para atención por OBRA SOCIAL o ingrese 2 para atención PARTICULAR"));
+            turnoIngresado = parseInt(prompt("Por favor, ingrese 1 para atención por OBRA SOCIAL o ingrese 2 para atención PARTICULAR."));
         }
 
         if (turnoIngresado === turno[0]) {
-            alert("Señor/a " + usuario + ", Usted a seleccionado atención por OBRA SOCIAL");
-            confirm("Usted sera atendido a la brevedad");
+            alert("Señor/a " + usuario + ", Usted ha seleccionado atención por OBRA SOCIAL.");
         }
         else if (turnoIngresado === turno[1]) {
-            alert("Señor/a " + usuario + ", Usted a seleccionado atención PARTICULAR");
-            confirm("Usted sera atendido a la brevedad");
+            alert("Señor/a " + usuario + ", Usted ha seleccionado atención PARTICULAR.");
         }
     } else {
-        alert("Señor/a " + usuario + ", Usted no a seleccionado ninguna opción válida");
+        alert("Señor/a " + usuario + ", Usted no a seleccionado ninguna opción válida.");
+    }
+}
+
+function determinarTurno() {
+    let decision;
+
+    if (turnoIngresado === turno[0] || turnoIngresado === turno[1]) {
+        decision = prompt('Por favor seleccione "A" para MEDICAMENTOS o "B" para OTRAS PRESTACIONES').toUpperCase();
+
+        if (decision === "A") {
+            alert("Usted será atendido a la brevedad por el sector de MEDICAMENTOS.");
+        } else if (decision === "B") {
+            alert("Usted será atendido a la brevedad por el sector de OTRAS PRESTACIONES.");
+        }else {
+            alert("Señor/a usted no ha seleccionado ninguna opción válida.");
+            determinarTurno();
+        } 
+    }
+}
+
+function confirmar() {
+    let confirmacion = confirm("¿Desea continuar?");
+
+    if (confirmacion) {
+        determinarTurno();
+    } else {
+        alert("Señor/a usted no ha seleccionado ninguna opción válida.");
     }
 }
 
 saludar();
+confirmar();
+
