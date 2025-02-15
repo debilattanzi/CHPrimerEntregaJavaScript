5fbdcfcf53cba4c40c9df68b4196f9bf6a20e40f
Debi Lattanzi <deboralattanzi@hotmail.com>
2025-02-15 19:46:46 -0300

    agregado de array de objetos, modificacion html y css

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
-function mostrarMensaje(mesaje){
+
+function mostrarMensaje(mensaje){
     let contenedorMensaje = document.getElementById("mensaje");
-    contenedorMensaje.innerHTML = mesaje;
+    contenedorMensaje.innerHTML = mensaje;
 }
 
 
@@ -13,51 +14,103 @@ function saludar() {
         return;
     }
 
-    if (turnoIngresado == parseInt(prompt("Ingrese 1 para atención por OBRA SOCIAL o ingrese 2 para atención PARTICULAR."))) {
+    mostrarMensaje("Bienvenido/a " + usuario+ ".<br>" +
+        "<button class='btn btn-primary mt-2' onclick=\"seleccionarTurno(1, '" + usuario + "')\">Obra Social</button> " +
+        "<button class='btn btn-secondary mt-2' onclick=\"seleccionarTurno(2, '" + usuario + "')\">Particular</button>");
 
-        while (turnoIngresado != turno[0] && turnoIngresado != turno[1]) {
-            alert("Por favor ingrese 1 o 2");
-            turnoIngresado = parseInt(prompt("Por favor, ingrese 1 para atención por OBRA SOCIAL o ingrese 2 para atención PARTICULAR."));
-        }
+}
 
-        if (turnoIngresado === turno[0]) {
-            alert("Señor/a " + usuario + ", Usted ha seleccionado atención por OBRA SOCIAL.");
+function seleccionarTurno(opcion, usuario) {
+    let eleccion
+
+    if (opcion === "1") {
+        eleccion = "OBRA SOCIAL";
+    } else if (opcion === "2") {
+        eleccion = "PARTICULAR";
+    } 
+
+    mostrarMensaje("Usted ha elegido " + eleccion + " para " + usuario + ".<br>" +
+        "<button class='btn btn-success mt-2' onclick='determinarTurno(\"A\")'>Medicamentos</button> " +
+        "<button class='btn btn-warning mt-2' onclick='determinarTurno(\"B\")'>Otras Prestaciones</button>");
+}
+    
+
+function determinarTurno(opcion) {
+
+
+        if (opcion === "A") {
+            
+            mostrarMedicamentos();
+        } else if (opcion === "B") {
+            mostrarPrestaciones();
         }
-        else if (turnoIngresado === turno[1]) {
-            alert("Señor/a " + usuario + ", Usted ha seleccionado atención PARTICULAR.");
-        }
+}
+
+let medicamentos = [
+    {nombre : 'Aspirina', precio : 100},
+    {nombre : 'Clonazepam', precio : 200},  
+    {nombre : 'Amoxicilina', precio : 300},
+    {nombre : 'Penicilina', precio : 400},
+    {nombre : 'Ciclosporina', precio : 500},
+    {nombre : 'Levotiroxina', precio : 600},
+    
+]
+
+function mostrarMedicamentos() {
+    let contenedorMedicamentos = document.getElementById("medicamentos");
+    contenedorMedicamentos.innerHTML = "";
+    for (let i = 0; i < medicamentos.length; i++) {
+        let medicamento = medicamentos[i];
+        let contenedorMedicamento = document.createElement("div");
+        contenedorMedicamento.className = "card";
+        contenedorMedicamento.innerHTML = "<div class='card-body'>" +
+            "<h5 class='card-title'>" + medicamento.nombre + "</h5>" +
+            "<p class='card-text'>Precio: $" + medicamento.precio + "</p>" +
+            "<button class='btn btn-primary mt-2' onclick='pedirMedicamento(\"" + medicamento.nombre + "\")'>Pedir</button>" +
+            "</div>";
+        contenedorMedicamentos.appendChild(contenedorMedicamento);
+    }
+    document.getElementById("mensaje").appendChild(contenedorMedicamentos);
+}
+   
+
+function pedirMedicamento(medicamento) {
+    mostrarMensaje("Usted ha pedido " + medicamento + "</b>."); 
+    
+    mostrarMedicamentos();
+} 
+
+let prestaciones = [
+    {nombre: 'Dermatologia'},
+    {nombre: 'Maquillaje'},
+    {nombre: 'Perfumeria'},
+]
+
+function mostrarPrestaciones() {
+    document.getElementById("busquedaPrestaciones").classList.remove("ocultar");
+    document.getElementById("busquedaPrestaciones").classList.add("mostrar");
+    document.getElementById("filtrar").addEventListener("click", function () {
+        let opcion = document.getElementById("filtroPrestaciones").value.trim().toLowerCase();
+
+        let prestacionesFiltradas = prestaciones.filter(prestacion =>
+            prestacion.nombre.toLowerCase().includes(opcion)
+        );
+        mostrarListadoPrestaciones(prestacionesFiltradas);
+    });
+}
+
+function mostrarListadoPrestaciones(prestacionesFiltradas) {
+    let contenedorPrestaciones = document.getElementById("prestaciones");
+    contenedorPrestaciones.innerHTML = "";  
+
+    if (prestacionesFiltradas.length > 0) {
+        prestacionesFiltradas.forEach(prestacion => {
+            let contenedorPrestacion = document.createElement("div");
+            contenedorPrestacion.className = "card2";
+            contenedorPrestacion.innerHTML = "<h5 class='card-title'> " + prestacion.nombre + "</h5>";
+            contenedorPrestaciones.appendChild(contenedorPrestacion);
+        });
     } else {
-        alert("Señor/a " + usuario + ", Usted no a seleccionado ninguna opción válida.");
+        contenedorPrestaciones.innerHTML = "<p>No se encontraron prestaciones con ese criterio.</p>";
     }
 }
-
-function determinarTurno() {
-    let decision;
-
-    if (turnoIngresado === turno[0] || turnoIngresado === turno[1]) {
-        decision = prompt('Por favor seleccione "A" para MEDICAMENTOS o "B" para OTRAS PRESTACIONES').toUpperCase();
-
-        if (decision === "A") {
-            alert("Usted será atendido a la brevedad por el sector de MEDICAMENTOS.");
-        } else if (decision === "B") {
-            alert("Usted será atendido a la brevedad por el sector de OTRAS PRESTACIONES.");
-        }else {
-            alert("Señor/a usted no ha seleccionado ninguna opción válida.");
-            determinarTurno();
-        } 
-    }
-}
-
-function confirmar() {
-    let confirmacion = confirm("¿Desea continuar?");
-
-    if (confirmacion) {
-        determinarTurno();
-    } else {
-        alert("Señor/a usted no ha seleccionado ninguna opción válida.");
-    }
-}
-
-saludar();
-confirmar();
-
